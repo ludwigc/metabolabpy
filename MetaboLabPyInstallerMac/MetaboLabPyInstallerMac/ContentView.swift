5cba7035415d67e1a8c2783fcce6e25f54a63d70
Christian Ludwig <ludwigc@mac.com>
2020-01-16 00:23:37 +0000

    mPy installer

---
+++ MetaboLabPyInstallerMac/MetaboLabPyInstallerMac/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MetaboLabPyInstallerMac
+//
+//  Created by Christian Ludwig on 27/11/2019.
+//  Copyright © 2019 ludwigsoft. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
