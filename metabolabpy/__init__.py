3ab9b64967838efb5455a1ab9aba03cee5463c95
Christian Ludwig <C.Ludwig@bham.ac.uk>
2020-08-20 11:17:07 +0100

    Version 0.6.4

--- metabolabpy/__init__.py
+++ metabolabpy/__init__.py
@@ -1,4 +1,4 @@
 __author__ = 'Christian Ludwig (C.Ludwig@bham.ac.uk)'
 __credits__ = 'Christian Ludwig (C.Ludwig@bham.ac.uk)'
-__version__ = '0.6.3'
+__version__ = '0.6.4'
 __license__ = 'GPLv3'
--- metabolabpy/__main__.py
+++ metabolabpy/__main__.py
@@ -98,7 +98,7 @@ class MplWidget2(QWidget):  # pragma: no cover
 
 class main_w(object):  # pragma: no cover
     def __init__(self):
-        self.__version__ = '0.6.3'
+        self.__version__ = '0.6.4'
         self.zoomWasOn = False
         self.panWasOn = False
         self.nd = nmrDataSet.NmrDataSet()
