7627fd0c9e93c7feaf88366a705435c4b3d57e60
Christian Ludwig <ludwigc@mac.com>
2021-05-09 01:59:13 +0100

    update

--- upload_pypi.sh
+++ upload_pypi.sh
@@ -7,3 +7,4 @@ twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 #pip install --index-url https://test.pypi.org/simple --extra-index-url https://pypi.org/simple metabolabpy==0.6.1
 
 conda deactivate 
+#comment
