a3b14509cf601b48e515d08cd827bc06e1b45c1e
Christian Ludwig <ludwigc@mac.com>
2023-03-16 12:11:28 +0000

    started isotopomer analysis

--- metabolabpy/iso/isotopomerAnalysis.py
+++ metabolabpy/iso/isotopomerAnalysis.py
@@ -1,4 +1,31 @@
 import os
 import pandas as pd
+import numpy as np
+import scipy as sp
+import os
+import math
+import time
+import metabolabpy.__init__ as ml_version
 
-#dd = pd.read_excel(excel_file, None)
+
+class IsotopomerAnalysis:
+
+    def __init__(self):
+        self.nmr_multiplets = pd.DataFrame()
+        self.ver = ml_version.__version__
+        # end __init__
+
+    def __str__(self):  # pragma: no cover
+        r_string = '______________________________________________________________________________________\n'
+        r_string += '\nMetaboLabPy Isotopomer Data Analysis (v. ' + self.ver + ')\n'
+        r_string += '______________________________________________________________________________________\n\n'
+        return r_string
+        # end __str__
+
+    def read_hsqc_multiplets(self, file_name=''):
+        if len(file_name) == 0:
+            return
+
+        self.nmr_multiplets = pd.read_excel(file_name, None)
+        return
+    # end read_hsqc_multiplets
