ecfd1c59a54416eefd9627e767afb5adc29bce85
Christian Ludwig <ludwigc@mac.com>
2023-04-01 21:50:40 +0100

    update

--- metabolabpy/nmr/analysis.py
+++ metabolabpy/nmr/analysis.py
@@ -8,3 +8,15 @@ def sat_rec(time_vector=[], intensity=0.0, rate=1.0):
     data_vector = np.copy(intensity*(1.0 - np.exp(-time_vector*rate)))
     return data_vector
     # end sat_rec
+
+def fct_sat_rec(fit_parameters=[], time_data=[], intensity_data=[]):
+    if len(fit_parameters) != 2 or len(time_data) == 0 or len(intensity_data) == 0 or len(time_data) != len(intensity_data):
+        return []
+
+    time_data = np.array(time_data)
+    intensity_data = np.array(intensity_data)
+    intensity = fit_parameters[0]
+    rate = fit_parameters[1]
+    sim_data = sat_rec(time_data, intensity, rate)
+    err = (sim_data - intensity_data)**2
+    return err.sum()
