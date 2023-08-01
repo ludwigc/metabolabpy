e2063d452843f6f6165a9f88f185c8d00daf9932
Christian Ludwig <ludwigc@mac.com>
2023-08-01 19:08:03 +0100

    update

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
-import unittest
+import unittest # pragma: no cover
 
-def suite():
-    test_loader = unittest.TestLoader()
-    test_suite  = test_loader.discover('.', pattern='test_nmrDataSet.py')
-    return test_suite
+def suite(): # pragma: no cover
+    test_loader = unittest.TestLoader() # pragma: no cover
+    test_suite  = test_loader.discover('.', pattern='test_nmrDataSet.py') # pragma: no cover
+    return test_suite # pragma: no cover
