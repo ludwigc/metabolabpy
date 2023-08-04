6c25fbaecab4bc8d2c6749bfe8a078c96b0094d7
Christian Ludwig <ludwigc@mac.com>
2023-08-04 12:28:58 +0100

    update

---
+++ tests/test_apcbc.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+
+"""
+
+test_acqPars
+
+author(s): C. Ludwig
+origin: 02-08-2023
+
+
+"""
+
+
+import unittest
+import metabolabpy.nmr.acqPars as acqPars
+import os
+
+
+class acqParsTestCase(unittest.TestCase):
+
+    def test_read_acq_pars(self):
+        pName = os.path.join(os.path.dirname(__file__), "data", "nmrData", "1")  # directory of spectrum
+        ap = acqPars.AcqPars()  # create nmrDataSet object
+        ap.read(pName)  # check if Bruker acqus data can be read
+
+    def test_read_v_acq_pars(self):
+        pName = os.path.join(os.path.dirname(__file__), "data", "nmrData", "varianData.fid")  # directory of spectrum
+        ap = acqPars.AcqPars()  # create nmrDataSet object
+        ap.read(pName)  # check if Varian procpar data can be read
+
+
+if __name__ == "__main__":
+    unittest.main()
