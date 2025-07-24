4142a012d55124f2cec253a61e886b20120db261
Christian Ludwig <ludwigc@mac.com>
2025-07-25 00:31:43 +0100

    HSQC script bug fix

--- metabolabpy/exampleScripts/example2DHSQCScript.py
+++ metabolabpy/exampleScripts/example2DHSQCScript.py
@@ -1,13 +1,13 @@
 kz = self.clear(True)
-data_path = '/Users/ludwigc/Desktop/nus_b600'	# select directory containing bruker data, interactive for file dialog
-data_sets = [101]										# add comma separated list of experiment numbers (e.g. [1, 2, 3])
+data_path = 'Replace with directory containing Bruker data'	# select directory containing bruker data, interactive for file dialog
+data_sets = []										# add comma separated list of experiment numbers (e.g. [1, 2, 3])
 self.read_spcs([data_path],data_sets)			# reading Bruker spectra
 lds = len(self.nd.nmrdat[0])
 for k in range(lds):									#
     self.nd.nmrdat[0][k].display.n_levels = 34					# setting number of contour levels
     self.nd.nmrdat[0][k].display.min_level = 0.01					# setting minimum contour level
     self.nd.nmrdat[0][k].display.max_level = 0.08					# setting maximum contour level
-    self.nd.nmrdat[0][k].proc.invert_matrix[1] = True
+    self.nd.nmrdat[0][k].proc.invert_matrix[1] = True					# adjust as necessary
 
 self.ft_all()
 self.nd.s = 0										# make first data set current data set
